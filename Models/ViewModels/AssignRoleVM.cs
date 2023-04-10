6da26fa949ddc18b10652ecfd67a7542f2da902d
SirYeast <mkorzhan@outlook.com>
2023-04-10 14:11:29 -0500

    Add seed data and admin controller

---
+++ Models/ViewModels/AssignRoleVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TaskManagementSystem.Areas.Identity.Data;
+
+namespace TaskManagementSystem.Models.ViewModels;
+
+public class AssignRoleVM
+{
+    public IEnumerable<ApplicationUser> Users { get; set; }
+    public IEnumerable<SelectListItem> Roles { get; set; }
+
+    public string Role { get; set; }
+}
