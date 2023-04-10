35ba643cc84e3141c0ea6d66ca20433d0534e503
brendantyler <galmanbrendan@gmail.com>
2023-04-10 12:26:50 -0500

    Add Models And Scaffolding

---
+++ Models/Projects.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagementSystem.Areas.Identity.Data;
+
+namespace TaskManagementSystem.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(200, MinimumLength = 5)]
+        public string Title { get; set; } = default!;
+        public HashSet<UserProjects> UserProjects { get; set; }
+        public HashSet<Tasks> Tasks { get; set; } = new HashSet<Tasks>();
+
+        public HashSet<ApplicationUser> Developers { get; set; } = new HashSet<ApplicationUser> { };
+    }
+}
---
+++ Models/UserProjects.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using TaskManagementSystem.Areas.Identity.Data;
+
+namespace TaskManagementSystem.Models
+{
+    public class UserProjects
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Project Manager")]
+        public ApplicationUser ProjectManager { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public Projects Project { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
+
