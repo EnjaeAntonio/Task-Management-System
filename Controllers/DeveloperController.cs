919a0b567e887740cc4b30c51050f1f95a363d2a
Enjae Antonio <Enjaeantonio@outlook.com>
2023-04-11 09:23:45 -0500

    View for MyProjects created

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TaskManagementSystem.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Build.Evaluation;
+using TaskManagementSystem.Models;
 
 namespace TaskManagementSystem.Controllers
 {
+    [Authorize(Roles = "Developer")]
     public class DeveloperController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationContext _context;
+
+        public DeveloperController(UserManager<ApplicationUser> userManager, ApplicationContext context)
         {
-            return View();
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<IActionResult> MyProjects()
+        {
+            ApplicationUser currentUser = await _userManager.GetUserAsync(User);
+
+            List<Projects> projects = _context.UserProjects
+                .Include(up => up.Project)
+                .Where(up => up.ApplicationUserId == currentUser.Id)
+                .Select(up => up.Project)
+                .ToList();
+
+            return View(projects);
         }
     }
 }
